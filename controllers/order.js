bb7ef090fb5cc60a6a1b0ed732cc5ad89f90704c
TOP-DEVELOPER <git@github.com:TOP-DEVELOPER/ecommerce-maket-place.git>
2020-05-19 21:58:54 -0400

    adding order

--- controllers/order.js
+++ controllers/order.js
@@ -1,17 +1,13 @@
-// require on top
-const sgMail = require('@sendgrid/mail');
-// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-require("dotenv").config();
-
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
 const { Order, CartItem } = require("../models/order");
-const { errorHandler } = require("../controllers/dbErrorHandler");
+const { errorHandler } = require("./dbErrorHandler");
 
+// sendgrid for email npm i @sendgrid/mail
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey('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');
 
 exports.orderById = (req, res, next, id) => {
     Order.findById(id)
-        .populate("products.product", "name price")
+        .populate('products.product', 'name price')
         .exec((err, order) => {
             if (err || !order) {
                 return res.status(400).json({
@@ -24,7 +20,7 @@ exports.orderById = (req, res, next, id) => {
 };
 
 exports.create = (req, res) => {
-    // console.log("CREATE ORDER: ", req.body);
+    console.log('CREATE ORDER: ', req.body);
     req.body.order.user = req.profile;
     const order = new Order(req.body.order);
     order.save((error, data) => {
@@ -33,14 +29,30 @@ exports.create = (req, res) => {
                 error: errorHandler(error)
             });
         }
+        // send email alert to admin
+        // order.address
+        // order.products.length
+        // order.amount
+        const emailData = {
+            to: 'kaloraat@gmail.com',
+            from: 'noreply@ecommerce.com',
+            subject: `A new order is received`,
+            html: `
+            <p>Customer name:</p>
+            <p>Total products: ${order.products.length}</p>
+            <p>Total cost: ${order.amount}</p>
+            <p>Login to dashboard to the order in detail.</p>
+        `
+        };
+        sgMail.send(emailData);
         res.json(data);
     });
 };
 
 exports.listOrders = (req, res) => {
     Order.find()
-        .populate("user", "_id name address")
-        .sort("-created")
+        .populate('user', '_id name address')
+        .sort('-created')
         .exec((err, orders) => {
             if (err) {
                 return res.status(400).json({
@@ -52,107 +64,16 @@ exports.listOrders = (req, res) => {
 };
 
 exports.getStatusValues = (req, res) => {
-    res.json(Order.schema.path("status").enumValues);
+    res.json(Order.schema.path('status').enumValues);
 };
 
 exports.updateOrderStatus = (req, res) => {
-    Order.update(
-        { _id: req.body.orderId },
-        { $set: { status: req.body.status } },
-        (err, order) => {
-            if (err) {
-                return res.status(400).json({
-                    error: errorHandler(err)
-                });
-            }
-            res.json(order);
-        }
-    );
-};
-
-
- 
-// your create order method with email capabilities
-exports.create = (req, res) => {
-    console.log('CREATE ORDER: ', req.body);
-    req.body.order.user = req.profile;
-    const order = new Order(req.body.order);
-    order.save((error, data) => {
-        if (error) {
+    Order.update({ _id: req.body.orderId }, { $set: { status: req.body.status } }, (err, order) => {
+        if (err) {
             return res.status(400).json({
-                error: errorHandler(error)
+                error: errorHandler(err)
             });
         }
-        // User.find({ categories: { $in: categories } }).exec((err, users) => {}
-        console.log('ORDER IS JUST SAVED >>> ', order);
-        // send email alert to admin
-        // order.address
-        // order.products.length
-        // order.amount
-        const emailData = {
-            to: 'amitabh.k@hotmail.com', // admin
-            from: 'noreply@ecommerce.com',
-            subject: `A new order is received`,
-            html: `
-            <h1>Hey Admin, Somebody just made a purchase in your ecommerce store</h1>
-            <h2>Customer name: ${order.user.name}</h2>
-            <h2>Customer address: ${order.address}</h2>
-            <h2>User's purchase history: ${order.user.history.length} purchase</h2>
-            <h2>User's email: ${order.user.email}</h2>
-            <h2>Total products: ${order.products.length}</h2>
-            <h2>Transaction ID: ${order.transaction_id}</h2>
-            <h2>Order status: ${order.status}</h2>
-            <h2>Product details:</h2>
-            <hr />
-            ${order.products
-                .map(p => {
-                    return `<div>
-                        <h3>Product Name: ${p.name}</h3>
-                        <h3>Product Price: ${p.price}</h3>
-                        <h3>Product Quantity: ${p.count}</h3>
-                </div>`;
-                })
-                .join('--------------------')}
-            <h2>Total order cost: ${order.amount}<h2>
-            <p>Login to your dashboard</a> to see the order in detail.</p>
-        `
-        };
-        sgMail
-            .send(emailData)
-            .then(sent => console.log('SENT >>>', sent))
-            .catch(err => console.log('ERR >>>', err));
- 
-        // email to buyer
-        const emailData2 = {
-            to: order.user.email,
-            from: 'noreply@ecommerce.com',
-            subject: `You order is in process`,
-            html: `
-            <h1>Hey ${req.profile.name}, Thank you for shopping with us.</h1>
-            <h2>Total products: ${order.products.length}</h2>
-            <h2>Transaction ID: ${order.transaction_id}</h2>
-            <h2>Order status: ${order.status}</h2>
-            <h2>Product details:</h2>
-            <hr />
-            ${order.products
-                .map(p => {
-                    return `<div>
-                        <h3>Product Name: ${p.name}</h3>
-                        <h3>Product Price: ${p.price}</h3>
-                        <h3>Product Quantity: ${p.count}</h3>
-                </div>`;
-                })
-                .join('--------------------')}
-            <h2>Total order cost: ${order.amount}<h2>
-            <p>Thank your for shopping with us.</p>
-        `
-        };
-        sgMail
-            .send(emailData2)
-            .then(sent => console.log('SENT 2 >>>', sent))
-            .catch(err => console.log('ERR 2 >>>', err));
- 
-        res.json(data);
+        res.json(order);
     });
 };
-
